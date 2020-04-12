098d7de75600604199d9da172cb160074d174cba
x-kaliber <tobulidis@gmail.com>
2020-04-12 21:38:11 +0100

    Smooth movement
    We have finished the movement of the Character.
    
    We are working in  setting up the enemy patterns and the code for the spawner

---
+++ WariorNurse/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public float increment;
+    public float MaxHeight;
+    public float MinHeight;
+    public float KeyDelay = 1f;
+    public int Health;
+
+    private float timePassed = 0f;
+    private Vector2 targetPos;
+
+
+
+    private void Update()
+    {
+        timePassed += Time.deltaTime;
+
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && timePassed > KeyDelay && transform.position.y < MaxHeight  || Input.GetKeyDown(KeyCode.W) && timePassed > KeyDelay && transform.position.y < MaxHeight )
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+            timePassed = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && timePassed > KeyDelay && transform.position.y > MinHeight || Input.GetKeyDown(KeyCode.S) && timePassed > KeyDelay && transform.position.y > MinHeight)
+        {
+
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+            timePassed = 0f;
+        }
+    }
+}
---
+++ WariorNurse/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    private float TimeBtwSpawn;
+    public float StartTimeBtwSpawn;
+    public float DecreaseTime;
+    public float MinTime = 0.65f;
+
+    public void Update()
+    {
+        if (TimeBtwSpawn <= 0)
+        {
+            Instantiate(Enemy, transform.position, Quaternion.identity);
+            TimeBtwSpawn = StartTimeBtwSpawn;
+            if (StartTimeBtwSpawn > MinTime)
+            {
+                StartTimeBtwSpawn -= DecreaseTime;
+            }
+        }
+        else
+        {
+            TimeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
